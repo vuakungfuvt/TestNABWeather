ca7390236295359d901b6b1259b3dae306129522
Tung Phan <tung.phan@doctoranywhere.com>
2022-04-08 15:32:18 +0700

    delete pod

--- Pods/Target Support Files/Pods-TestNABWeather-TestNABWeatherUITests/Pods-TestNABWeather-TestNABWeatherUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestNABWeather_TestNABWeatherUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TestNABWeather_TestNABWeatherUITests
-@end
--- Pods/Target Support Files/Pods-TestNABWeather/Pods-TestNABWeather-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestNABWeather : NSObject
-@end
-@implementation PodsDummy_Pods_TestNABWeather
-@end
